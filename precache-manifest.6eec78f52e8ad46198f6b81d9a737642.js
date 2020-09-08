3cedb652d8058bd00a2c1a0853ec1033dc77ae72
ChrisStone07 <chrischris_4425@hotmail.com>
2020-09-08 16:08:37 +0530

    Updates

---
+++ precache-manifest.6eec78f52e8ad46198f6b81d9a737642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d1743037728ae85bb250d22174e8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3fedba4d82081dbc5b9",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fedba4d82081dbc5b9",
+    "url": "/static/js/main.5e928c3a.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
